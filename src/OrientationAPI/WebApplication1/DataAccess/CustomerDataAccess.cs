56a26182727445c3138885901e556268a7233507
Latasha Simmons <lsimmons1832@gmail.com>
2017-09-05 21:22:49 -0500

    added DataAccess folder and CustomerDataAccess file with seed data

---
+++ src/OrientationAPI/WebApplication1/DataAccess/CustomerDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.DataAccess
+{
+    public class CustomerDataAccess
+    {
+        public class IntRepository : IRepository<int>
+        {
+            public List<int> GetAll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+    }
+}
