1ff60d5a5acad473a1cae825334fa57584b00ea4
John McCutchan <john.mccutchan.wd@gmail.com>
2017-09-07 20:48:50 -0500

    Fixed Script
    This one is good, I swear.

--- sql/201709072019_createProductTable.sql
+++ sql/201709072019_createProductTable.sql
@@ -12,10 +12,10 @@ CREATE TABLE [dbo].[Product](
 	[ProductId] [int] IDENTITY(1,1) NOT NULL,
 	[ProductName] [varchar](40) NOT NULL,
 	[ProductDescription] [varchar](40) NOT NULL,
-	[ProductPrice] [int](15) NOT NULL,
+	[ProductPrice] [int] NOT NULL,
  CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
 (
-	[CustomerId] ASC
+	[ProductId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
