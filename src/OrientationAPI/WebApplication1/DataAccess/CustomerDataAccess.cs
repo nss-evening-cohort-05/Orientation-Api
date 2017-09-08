752ab5bdaa54e8798683d9b8fedfcefec442a677
Latasha Simmons <lsimmons1832@gmail.com>
2017-09-07 20:14:25 -0500

    adding method to inactivate customer

--- src/OrientationAPI/WebApplication1/DataAccess/CustomerDataAccess.cs
+++ src/OrientationAPI/WebApplication1/DataAccess/CustomerDataAccess.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+
 
 namespace WebApplication1.DataAccess
 {
@@ -13,11 +16,27 @@ namespace WebApplication1.DataAccess
             {
                 throw new NotImplementedException();
             }
+
+            public void InactivateCustomer(int entitytoUpdate)
+            {
+                var newStatus = 0;
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Bangazon"].ConnectionString))
+                {
+                    connection.Open();
+
+                    var result = connection.QueryFirstOrDefault<CustomerListResult>(
+                        "Update Customer set Active = @newStatus Where CustomerId = @CustomerId",
+                        new { CustomerId = @entitytoUpdate, Active = @newStatus });
+
+                }
+            }
+
         }
     }
 
     public interface IRepository<T>
     {
         List<T> GetAll();
+        void InactivateCustomer(T entityToUpdate);
     }
 }
