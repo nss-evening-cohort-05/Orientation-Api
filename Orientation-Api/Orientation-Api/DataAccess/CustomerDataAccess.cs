15c859024a1f23b54f5bf02287a138f7d49bb48f
Sanyyouisf <sanymamdoh@yahoo.com>
2017-09-08 23:22:06 -0700

    make the Customer Inactive

---
+++ Orientation-Api/Orientation-Api/DataAccess/CustomerDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using Dapper;
+using Orientation_Api.Models;
+
+namespace Orientation_Api.DataAccess
+{
+    public class CustomerDataAccess
+    {
+    //------------------------------------------------------------------------
+        //get all the customer data
+        public List<CustomerList> GetAllCustomers()
+        {
+            using (var Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Bangazon"].ConnectionString))
+            {
+                Connection.Open();
+                var result = Connection.Query<CustomerList>("select * from Customer");
+                return result.ToList();
+            }
+        }
+    //------------------------------------------------------------------------
+        //update the customer state to be Inactive by passing the customerId
+        public void CustomerInactive(int CustomerId)
+        {
+             using ( var Connection = new SqlConnection (ConfigurationManager.ConnectionStrings["Bangazon"].ConnectionString))
+             {
+                Connection.Open();
+                var Result = Connection.Execute("update Customer set Active = 'false' where CustomerId = @Id",
+                                                  new { Id = CustomerId });
+             }
+        }
+    //--------------------------------------------------------------------------
+    }
+}
---
+++ Orientation-Api/Orientation-Api/Models/CustomerList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Orientation_Api.Models
+{
+    public class CustomerList
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName {get; set; }
+        public string Adress { get; set; }
+        public Boolean Active { get; set; }
+        public int telephone { get; set; }
+        public string email { get; set; }
+    }
+}
