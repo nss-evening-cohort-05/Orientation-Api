b9af4714720376d82beaf829917fe9b90b0c8328
Latasha Simmons <lsimmons1832@gmail.com>
2017-09-11 19:20:54 -0500

    created stage files for products

---
+++ src/OrientationAPI/WebApplication1/DataAccess/ProductDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.DataAccess
+{
+    public class ProductDataAccess : IProductRepository<ProductListResult>
+    {
+        public List<ProductListResult> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductListResult> CreateProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductListResult> DeleteProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MarkOutOfStock(int entityToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface IProductRepository<T>
+    {
+        List<T> GetAllProducts();
+        List<T> CreateProduct();
+        List<T> DeleteProduct();
+        bool MarkOutOfStock(int entityToUpdate);
+    }
+}
---
+++ src/OrientationAPI/WebApplication1/Models/ProductListResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ProductListResult
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
