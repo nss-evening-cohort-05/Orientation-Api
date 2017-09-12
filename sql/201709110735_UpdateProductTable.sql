64515b4becc6dcdf2cfd3cde310929c7fd983572
Andrea Finch <aefinch@vt.edu>
2017-09-11 21:12:44 -0500

    revised UpdateProductTable and added CreateOrdersTable script

--- sql/201709110735_UpdateProductTable.sql
+++ sql/201709110735_UpdateProductTable.sql
@@ -1,12 +1,2 @@
-USE [Bangazon]
-GO
-
-UPDATE [dbo].[Product]
-   SET [ProductName] = <ProductName, varchar(40),>
-      ,[ProductDescription] = <ProductDescription, varchar(40),>
-      ,[ProductPrice] = <ProductPrice, money,>
-      ,[CurrentInventory] = <CurrentInventory, int,>
- WHERE <Search Conditions,,>
-GO
-
-
+alter table Product
+add CurrentInventory int
