b8ee9c64ecc32afe004c66b832c97c73903d1efe
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-09-11 20:14:39 -0500

    add new product

--- Orientation-Api/Orientation-Api/Controllers/ProductController.cs
+++ Orientation-Api/Orientation-Api/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Orientation_Api.DataAccess;
+using Orientation_Api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,32 +12,26 @@ namespace Orientation_Api.Controllers
     [RoutePrefix("api/Product")]
     public class ProductController : ApiController
     {
-        // GET: api/Product
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Product/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Product
+        
+        // POST: Add new product  Route: api/Product/newProduct 
         [HttpPost, Route("newProduct")]
-        public void Post([FromBody]string value)
+        public HttpResponseMessage AddNewProduct(Product product)
         {
-        }
+            try
+            {
+                var productDatabase = new ProductDatabase();
+                if (productDatabase.newProduct(product) > 0)
+                    return Request.CreateResponse(HttpStatusCode.OK, "New product added");
+                else
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Error add new product"); 
+            }
+            catch (Exception ex)
+            {
 
-        // PUT: api/Product/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Server Error" , ex);
+            }
 
-        // DELETE: api/Product/5
-        public void Delete(int id)
-        {
         }
+               
     }
 }
---
+++ Orientation-Api/Orientation-Api/DataAccess/ProductDatabase.cs
@@ -0,0 +1,44 @@
+﻿using Orientation_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace Orientation_Api.DataAccess
+{
+    public class ProductDatabase
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["Bangazon"].ConnectionString;
+
+        public int newProduct(Product product)
+        {
+            var sql = @"INSERT INTO [dbo].[Product]
+                                   (ProductName
+                                   ,ProductPrice
+                                   ,ProductDescription
+                                   ,Inventory)
+                             VALUES
+                                   (@ProductName
+                                   ,@ProductPrice
+                                   ,@ProductDescription
+                                   ,@Inventory)";
+
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+                return connection.Execute(sql, new
+                {
+                    ProductName       = product.ProductName
+                   ,ProductPrice      = product.ProductPrice
+                   ,ProductDescription = product.ProductDescription
+                   ,Inventory         = product.Inventory
+                });
+
+            }
+        }
+    }
+}
